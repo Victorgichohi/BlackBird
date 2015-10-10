ca4dcf0097a8b691848d7842bcf8046c61c9e422
victorgichohi <victorgichohi48@gmail.com>
2015-10-10 14:29:51 +0300

    these is the actual function showing scroll position

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,9 @@
 $(window).scroll(function(){
 
   //this function tells how far the postion one has scrolled from the top
-  var wScoll=$(this).scrollTop();
+  var wScroll=$(this).scrollTop();
   // THIS WILL HELP IN SHOWING THE AMOUNT ONE HAS SCROLLED
-  console.log('hello')
+  console.log(wScroll)
 
 
 });
